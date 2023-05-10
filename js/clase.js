69a8d1974a32f6bd0e48205df8d607f1e673de45
OscarIvanBecerra <oibecerrao@ufpso.edu.co>
2023-05-10 17:00:54 -0500

    Aprendiendo js

---
+++ js/clase.js
@@ -0,0 +1,45 @@
+// VARIABLES 
+const nameuser = "OscarIvanBecerra"
+var x = "Valor";
+let y = "Valor";
+
+//19:20
+
+let a = 20;
+let b = 5.4;
+let string = "Mi nombre es x";
+let number = "0123456789";
+let boolean_uno = false;
+let boolean_two = true;
+let array = [1,2,3,4,5];
+let array_string = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+
+// OPERADORES ARITMÉTICOS + - / * %
+
+let suma = 1+5+a;
+let resta = 1-5-a;
+let multi = 1 * 5 *a;
+let divi = 1 / 5 /a;
+let module = 10%2;
+let elev = 5^2;
+console.log(" \n- " + suma + " \n- " + resta + " \n- " + multi + " \n- " + divi + " \n- " + module + " \n- " + elev )
+
+// OPERADORES DE COMPARACIÓN = / == / === / <> / >= / <= / > / <
+
+// OPERADORES LÓGICOS AND && / OR || / 
+
+if(10>5){
+    for(let i=0;i<array_string.length;i++){
+        console.log(array_string[i]);
+
+    }
+}
+
+let j = 5;
+let k = 10;
+while(k>=j){
+    console.log("##### ENTRA #####");
+    j++;
+    break
+}
+
